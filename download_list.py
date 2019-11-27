3204a2f928c01d8474044478e75a89537246bcd8
Jonathan Satterfield <jonathansatt@gmail.com>
2019-11-26 21:58:58 -0600

    Removed obsolete scripts

--- download_list.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-ex_request = requests.get("https://raw.githubusercontent.com/austinheap/sophos-xg-block-lists/master/adguard.txt")
-if ex_request.status_code != 200:
-    print("failure") # later substitute to log or notify owner
-else:
-    ex_list = ex_request.text.split()
-    print(ex_list)
