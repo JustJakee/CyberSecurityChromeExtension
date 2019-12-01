00c68c3610333cfb68bf93837b12a28492c9675a
Senay Teclebrhan <senay_berihu@outlook.com>
2019-11-30 19:25:04 -0600

    Commented our Python code.

--- create_master_list.py
+++ create_master_list.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 
+# These are the domains of the different filter lists we use.
 input_lists =  [("https://1hos.cf/complete/hosts.txt", "1Hosts"), \
                 ("https://raw.githubusercontent.com/notracking/hosts-blocklists/master/domains.txt", "NoTracking"), \
                 ("https://raw.githubusercontent.com/austinheap/sophos-xg-block-lists/master/nocoin.txt", "NoCoin"), \
@@ -9,15 +10,17 @@ input_lists =  [("https://1hos.cf/complete/hosts.txt", "1Hosts"), \
                 ("https://raw.githubusercontent.com/austinheap/sophos-xg-block-lists/master/coinblocker.txt", "CoinBlocker"), \
                 ("https://raw.githubusercontent.com/austinheap/sophos-xg-block-lists/master/adguard.txt", "AdGuard") ]
 
+# This for loop writes each domain of each list into a file.
 for list in input_lists:
     ex_request = requests.get(list[0])
     if ex_request.status_code != 200:
-        print("failed to download list from" + list_url) # later substitute to log or notify owner
+        print("failed to download list from" + list) # later substitute to log or notify owner
     else:
         f = open("filterlists/" + list[1] + ".txt", 'w')
         f.write(ex_request.text);
         f.close()
 
+# Formats the domains in 1Hosts.txt to match our standard.
 with open('filterlists/1Hosts.txt', 'r+') as OneHosts:
     for i in range(25):
         OneHosts.readline()
@@ -25,7 +28,8 @@ with open('filterlists/1Hosts.txt', 'r+') as OneHosts:
     OneHosts.seek(0)
     OneHosts.write(re.sub('^\S+ (.*)', '\g<1>', content, count=0, flags=re.M))
     OneHosts.truncate()
-    
+
+# Formats the domains in NoTracking.txt to match our standard.
 with open('Filterlists/NoTracking.txt', 'r+') as NoTracking:
     for i in range(15):
         NoTracking.readline()
@@ -34,6 +38,7 @@ with open('Filterlists/NoTracking.txt', 'r+') as NoTracking:
     NoTracking.write(re.sub('^address=\/(.*?)\/.*', '\g<1>', content, count=0, flags=re.M))
     NoTracking.truncate()
 
+# Creates a set() of all the domains in all the txt files.
 dir = "Filterlists"
 master_set = set()
 for file in os.listdir(dir):
@@ -42,10 +47,12 @@ for file in os.listdir(dir):
         master_set.add(line)
     f.close()
 
+# Further formats each domain in the set to match the standards of the Chrome APIs and then adds it to a list.
 master_list = []
 for ele in master_set:
    master_list.append("*://*." + str(ele).strip() + "/*")
 
+# Writes the fully formatted domains to MASTER_FILTER_LIST.txt which will be transferred to our JavaScript code.
 with open('MASTER_FILTER_LIST.txt', 'w') as master:
     master.write(str(master_list))
 
