1e5bfb067038fb3ba62e0fc01717cdfe84395090
senaybee <senay_berihu@outlook.com>
2019-12-01 18:36:19 -0600

    Commented the javascript code

--- background.js
+++ background.js
@@ -1,3 +1,4 @@
+//This script listens for requests to websites that are in blocked_domains.js and blocks them.
 chrome.browserAction.setBadgeText({ text: "ON" });
 var enabled = true;
 chrome.webRequest.onBeforeRequest.addListener(
